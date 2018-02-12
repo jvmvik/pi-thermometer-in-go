4f0d514b5be64d5f58272b06a77ebf686855592f
jvmvik <jvmvik@gmail.com>
2018-02-11 21:45:06 -0600

    Show live data

--- spa_monitor.go
+++ spa_monitor.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+    "encoding/csv"
 	//"errors"
 	"fmt"
 	"io/ioutil"
@@ -12,6 +13,9 @@ import (
 	"strconv"
 	"strings"
     "html/template"
+    "time"
+    "os"
+    "bufio"
 )
 
 // Group of measurement
@@ -77,11 +81,8 @@ func Round(x, unit float32) float32 {
 	return float32(int32(x/unit+0.5)) * unit
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-    root := "data/"
-    
-    // Current value
-	datapoint := ReadDatapoint(root) 
+func ReadDisplay(root string) DisplayThermometer {
+    datapoint := ReadDatapoint(root) 
     var display DisplayThermometer
     for _, item := range datapoint {
         if(item.ID == "28-0517c1a38eff") {
@@ -92,16 +93,75 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
         }
     }
     
-    // TODO Read water and air history
+    return display
+}
+
+func ReadHistory(path string, display *DisplayThermometer) {
+    
+    file, err := os.Open("history.csv")
+    if err != nil {
+        log.Fatal("cannot open file")
+    }
+    defer file.Close()
+    
+    r := csv.NewReader(bufio.NewReader(file))
+
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+    fmt.Println(">>")
+	fmt.Print(records)
+    
+    
+     // TODO Read water and air history
     display.HistoryAir = "0,223 48,138.5 154.7,169 211,88.5 294.5,80.5 380,165.2 437,75.5 469.5,223.3"
     
-    display.HistoryWater = "0,223 48,138.5 154.7,169 211,88.5 294.5,80.5 380,165.2 437,75.5 469.5,223.3"
+    display.HistoryWater = "0,223 0,50 48,120.5 154.7,169 211,88.5 294.5,80.5 380,165.2 437,75.5 469.5,223.3"
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    if(r.URL.Path != "/") {
+        http.NotFound(w, r)
+        return
+    }
+    root := "data/"
     
+    // Current value to display
+    display := ReadDisplay(root)
+    ReadHistory("history.csv", &display)
     t, _ := template.ParseFiles("index.html")
     t.Execute(w, display)
 }
 
 func recordHandler(w http.ResponseWriter, r *http.Request) {
+    
+    p := ReadDisplay("data/")
+    
+    // header {"air", "water", "timestamp"}
+    record := []string{strconv.FormatFloat(float64(p.Air), 'f', -1, 32),
+                       strconv.FormatFloat(float64(p.Water), 'f', -1, 32),
+                       strconv.FormatInt(time.Now().Unix(), 10)}
+
+    file, err := os.OpenFile("history.csv", os.O_APPEND|os.O_WRONLY, 0600)
+    if err != nil {
+        log.Fatal("cannot open file")
+    }
+    defer file.Close()
+
+    writer := csv.NewWriter(file)
+    if err := writer.Write(record); err != nil {
+        log.Fatalln("error writing record to csv:", err)
+    }
+    writer.Flush()
+    
+    if err := writer.Error(); err != nil {
+		log.Fatal(err)
+        fmt.Fprintf(w, "{\"status\":\"fail\"}")
+        return
+	}
+    
     fmt.Fprintf(w, "{\"status\":\"ok\"}")
 }
 
@@ -109,6 +169,8 @@ func main() {
 	fmt.Println("start web server.\n http://spa")
     h := http.NewServeMux()
 	h.HandleFunc("/json", jsonHandler)
+    
+    h.HandleFunc("/record", recordHandler)
     
     fs := http.FileServer(http.Dir("static"))
     h.Handle("/static/", http.StripPrefix("/static/", fs))
